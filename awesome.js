d842acf3ef65163bf04ac237d93a9e65979209da
dan <dan@micro.(none)>
2010-11-18 22:45:42 -0800

    use a better method for obtaining window height for the lightbox

--- awesome.js
+++ awesome.js
@@ -185,7 +185,11 @@ var AWESOME = (function () {
 		},
 		docHeight: function () {
 			var D = document;
-			return Math.max(D.body.clientHeight, D.documentElement.clientHeight);
+			return Math.max(
+				Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+				Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+				Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+			);
 		},
 		docWidth: function () {
 			var D = document;
