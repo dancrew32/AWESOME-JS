c8a4c5dfaecdf02130e1d3544011c3de301f44a7
Dan Masquelier <dancrew32@gmail.com>
2011-04-22 22:40:26 -0700

    adding lightweight xml and json parsers var obj = $.parse(str, type); where type is 'xml' or 'json' (default)

--- awesome.js
+++ awesome.js
@@ -280,8 +280,8 @@ var AWESOME = (function () {
 		remove: function (ele) {
 			if (!ele) return false;
 			if (!('length' in ele)) {
-    			ele = [ele];
-    		}
+				ele = [ele];
+			}
 			var i = ele.length;
 			while (i--) {
 				if (typeof ele[i].parentNode !== 'undefined') {
@@ -325,11 +325,11 @@ var AWESOME = (function () {
 		// Math
 		getMax: function (array) {
 			var m = Math;
-		    return m.max.apply(m, array);
+			return m.max.apply(m, array);
 		},
 		getMin: function (array) {
 			var m = Math;
-		    return m.min.apply(m, array);
+			return m.min.apply(m, array);
 		},
 		getRandom: function(min, max) {
 			var m = Math;
@@ -481,6 +481,130 @@ var AWESOME = (function () {
 				}
 			}
 			return options;
+		},
+		parse: function(str, type) {
+			type = type.toLowerCase() || 'json';	
+			var result;
+			switch (type) {
+				case 'xml':
+					if (window.DOMParser) {
+						var parser = new DOMParser();
+						result = parser.parseFromString(str, 'text/xml');
+					} else { // ie
+						var xmlDoc = new ActiveXObject('Microsoft.XMLDOM');
+						xmlDoc.async = 'false';
+						result = xmlDoc.loadXML(str); }
+				break;
+				case 'json':
+					var number = '(?:-?\\b(?:0|[1-9][0-9]*)(?:\\.[0-9]+)?(?:[eE][+-]?[0-9]+)?\\b)';
+					var oneChar = '(?:[^\\0-\\x08\\x0a-\\x1f\"\\\\]'
+							+ '|\\\\(?:[\"/\\\\bfnrt]|u[0-9A-Fa-f]{4}))';
+					var string = '(?:\"' + oneChar + '*\")';
+					var jsonToken = new RegExp(
+							'(?:false|true|null|[\\{\\}\\[\\]]'
+								+ '|' + number
+								+ '|' + string
+								+ ')', 'g');
+					var escapeSequence = new RegExp('\\\\(?:([^u])|u(.{4}))', 'g');
+					var escapes = {
+						'"': '"',
+						'/': '/',
+						'\\': '\\',
+						'b': '\b',
+						'f': '\f',
+						'n': '\n',
+						'r': '\r',
+						't': '\t'
+					};
+					function unescapeOne(_, ch, hex) {
+						return ch ? escapes[ch] : String.fromCharCode(parseInt(hex, 16));
+					}
+					var EMPTY_STRING = new String('');
+					var SLASH = '\\';
+					var firstTokenCtors = { '{': Object, '[': Array };
+					var hop = Object.hasOwnProperty;
+
+					var toks = str.match(jsonToken);
+					var tok = toks[0];
+					var topLevelPrimitive = false;
+					if ('{' === tok) {
+						result = {};
+					} else if ('[' === tok) {
+						result = [];
+					} else {
+						result = [];
+						topLevelPrimitive = true;
+					}
+					var key;
+					var stack = [result];
+					for (var i = 1 - topLevelPrimitive, n = toks.length; i < n; ++i) {
+						tok = toks[i];
+						var cont;
+						switch (tok.charCodeAt(0)) {
+							default:  // sign or digit
+								cont = stack[0];
+								cont[key || cont.length] = +(tok);
+								key = void 0;
+								break;
+							case 0x22:  // '"'
+								tok = tok.substring(1, tok.length - 1);
+								if (tok.indexOf(SLASH) !== -1) {
+									tok = tok.replace(escapeSequence, unescapeOne);
+								}
+								cont = stack[0];
+								if (!key) {
+									if (cont instanceof Array) {
+										key = cont.length;
+									} else {
+										key = tok || EMPTY_STRING;  // Use as key for next value seen.
+										break;
+									}
+								}
+								cont[key] = tok;
+								key = void 0;
+								break;
+							case 0x5b:  // '['
+								cont = stack[0];
+								stack.unshift(cont[key || cont.length] = []);
+								key = void 0;
+								break;
+							case 0x5d:  // ']'
+								stack.shift();
+								break;
+							case 0x66:  // 'f'
+								cont = stack[0];
+								cont[key || cont.length] = false;
+								key = void 0;
+								break;
+							case 0x6e:  // 'n'
+								cont = stack[0];
+								cont[key || cont.length] = null;
+								key = void 0;
+								break;
+							case 0x74:  // 't'
+								cont = stack[0];
+								cont[key || cont.length] = true;
+								key = void 0;
+								break;
+							case 0x7b:  // '{'
+								cont = stack[0];
+								stack.unshift(cont[key || cont.length] = {});
+								key = void 0;
+								break;
+							case 0x7d:  // '}'
+								stack.shift();
+								break;
+						}
+					}
+					if (topLevelPrimitive) {
+						if (stack.length !== 1) { throw new Error(); }
+						result = result[0];
+					} else {
+						if (stack.length) { throw new Error(); }
+					}
+				break;
+			}
+			return result;
 		},
 		ajax: function(options) {
 			options = this.setDefaults({
